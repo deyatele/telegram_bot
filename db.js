eb408654a5caaf5034e017a02176035b40733600
deyatele <deyatele@gmail.com>
2021-09-27 00:41:03 +0300

    DATA_BASE

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require('sequelize');
 //const { DATABASE_URL } = require('./procesEnv');
 const urlDB = process.env.DATABASE_URL
-module.exports = new Sequelize(urlDB) 
-//     {
-//   //logging: false,// отключение логирования
-//   ssl: true,
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// })
-;
+module.exports = new Sequelize(urlDB, 
+    {
+  //logging: false,// отключение логирования
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+}
+);
