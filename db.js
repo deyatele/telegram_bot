85f6621b4bfbe0c37106fdca378dc0d2d4ecd2a2
deyatele <deyatele@gmail.com>
2021-09-27 00:30:26 +0300

    DATA_BASE

--- db.js
+++ db.js
@@ -1,8 +1,6 @@
 const { Sequelize } = require('sequelize');
-const {
-    DATABASE_URL
-} = require('./procesEnv');
-
+//const { DATABASE_URL } = require('./procesEnv');
+const urlDB = process.env.DATABASE_URL
 module.exports = new Sequelize(DATABASE_URL, {
   //logging: false,// отключение логирования
   ssl: true,
